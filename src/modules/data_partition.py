53dab85c1665fa01724c92f569f6ab1c89a0a67e
sitaomin1994 <sitaomin@usc.edu>
2023-07-27 06:18:24 -0400

    sample evenly

--- src/modules/data_partition.py
+++ src/modules/data_partition.py
@@ -12,6 +12,24 @@ def data_partition(strategy, params, data, n_clients, seed=201030, regression=Fa
 	print(strategy, params)
 	if strategy == 'full':
 		return [data.copy() for _ in range(n_clients)]
+	elif strategy == 'sample-evenly':
+		sample_fracs = [1 / n_clients for _ in range(n_clients)]
+		ret = []
+		for idx, sample_frac in enumerate(sample_fracs):
+			new_seed = seed + idx * seed + 990983
+			# new_seed = seed
+			if regression:
+				_, X_test, _, y_test = train_test_split(
+					data[:, :-1], data[:, -1], test_size=sample_frac,
+					random_state=(new_seed) % (2 ** 32)
+				)
+			else:
+				_, X_test, _, y_test = train_test_split(
+					data[:, :-1], data[:, -1], test_size=sample_frac,
+					random_state=(new_seed) % (2 ** 32), stratify=data[:, -1]
+				)
+			ret.append(np.concatenate([X_test, y_test.reshape(-1, 1)], axis=1).copy())
+		return ret
 	elif strategy == 'sample':
 		sample_frac = int(params['p']) / data.shape[0]
 		print(sample_frac, data.shape[0])
