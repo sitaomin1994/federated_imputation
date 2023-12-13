89a831af00dc651cdf22d8b8786330387fe08d33
sitao <sitaomin@usc.edu>
2023-12-13 15:54:31 -0500

    remove unnecessary code

--- src/missing_data_exploration/sub_modules/tests/test_data_spliting.py
+++
@@ -1,9 +0,0 @@
-from ..data_spliting import partition_data
-import pandas as pd
-import numpy as np
-
-
-def test_partition_data():
-	dataset = pd.DataFrame(np.random.randint(0, 10, (100, 10))).values
-	ret = partition_data(dataset, 5)
-	assert True
