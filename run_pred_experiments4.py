7d96a650a8ed37206a3bc483627cce843aa5e2fd
sitaomin1994 <sitaomin@usc.com>
2024-02-20 15:37:01 -0500

    pc1

--- run_pred_experiments4.py
+++ run_pred_experiments4.py
@@ -130,7 +130,7 @@ def prediction(main_config, server_config_, pred_rounds, seed, mtp=False, method
                         rets.append(ret)
                 else:
                     n_process = n_rounds
-                    if n_process > 10: n_process = 10
+                    if n_process > 10: n_process = 5
                     chunk_size = n_rounds // n_process
                     rounds = list(range(n_rounds))
 
@@ -251,7 +251,7 @@ if __name__ == '__main__':
     pred_rounds = 1
     seed = 21
     mtp = True
-    datasets = ['fed_imp_ext_pc2/0216/mimiciii_mo2']
+    datasets = ['fed_imp_pc1/0216/mimiciii_mo2']
     train_params = [
         # {"num_hiddens": 32, "batch_size": 300, "lr": 0.001, "weight_decay": 0.000, 'imbalance': None},
         # {"num_hiddens": 32, "batch_size": 300, "lr": 0.001, "weight_decay": 0.000, 'imbalance': None},
@@ -279,7 +279,7 @@ if __name__ == '__main__':
             ]  # "random2@mrl=0.2_mrr=0.8_mm=mnarlrq"]
 
             main_config = copy.deepcopy(main_config_tmpl)
-            main_config["n_rounds"] = 30
+            main_config["n_rounds"] = 20
             main_config['data'] = dataset
             main_config['n_clients'] = n_clients
             main_config['sample_size'] = sample_size
